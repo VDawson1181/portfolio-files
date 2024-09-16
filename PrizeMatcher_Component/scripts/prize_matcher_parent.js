c2065163379dff5c25f676843ead82bad789434a
Val dawson <vdawson647@gmail.com>
2024-09-15 23:24:56 -0400

    taking out origin check for testing...

--- PrizeMatcher_Component/scripts/prize_matcher_parent.js
+++ PrizeMatcher_Component/scripts/prize_matcher_parent.js
@@ -1,6 +1,6 @@
 // Description: This script is used to listen for messages from the iframe and redirect the parent window to the prize page.
 window.addEventListener("message", ev => {    
-    if(ev.origin !== "http://127.0.0.1:5500/") return; //Check origin for security..... if necessary.
+    // if(ev.origin !== "http://127.0.0.1:5500/") return; //Check origin for security..... if necessary.
 
     console.log(ev.data);
     window.location.replace(ev.data);
